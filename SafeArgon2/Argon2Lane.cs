20dcc575fcf6fc1f5d32ab109dcdfa8f868963b6
Ivan Kizenko <hellobody0@gmail.com>
2025-04-15 15:02:31 +0300

    Argon2Lane class added.

---
+++ SafeArgon2/Argon2Lane.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace SafeArgon2
+{
+    public class Argon2Lane
+    {
+        public Argon2Lane(int blockCount)
+        {
+            _memory = new ulong[128 * blockCount];
+
+            BlockCount = blockCount;
+        }
+
+        public ulong[] this[int index]
+        {
+            get
+            {
+                if (index < 0 || index > BlockCount)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(index));
+                }
+
+                // TODO: Possibly optimization required.
+                ulong[] slice = new ulong[128];
+
+                Array.Copy(_memory, 128*index, slice, 0, 128);
+
+                return slice;
+            }
+        }
+
+        public int BlockCount { get; }
+
+        private readonly ulong[] _memory;
+    }
+}
